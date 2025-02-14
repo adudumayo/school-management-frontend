48c7bc8b52abe65ea8d02891489a5dcfa75d8f07
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-14 21:57:59 +0200

    fetch all learners from the backend and render the list (basic)

--- src/LearnersList.jsx
+++ src/LearnersList.jsx
@@ -1,11 +1,26 @@
 import { useEffect, useState } from "react";
 
 function LearnersList() {
+  const [learners, setLearners] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/learners")
+      .then((res) => res.json())
+      .then((data) => setLearners(data))
+      .catch((err) => console.error("Error fetching learners:", err));
+  }, []);
+
   return (
-    <ul>
-      <li>This can't be right</li>
-    </ul>
+    <div>
+      <h2>List of Learners</h2>
+      <ul>
+        {learners.map((learner) => (
+          <li key={learner.id}>{learner.name} is the name</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
 export default LearnersList;
+
