582c5382e5100e3ba519c004ad1fe4b889b2ec61
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-04-03 10:53:44 +0200

    I have removed the test cluster from the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import './App.css';
-import LearnersList from './components/admin/LearnersList';
-import AddLearnerForm from './components/admin/AddLearnerForm';
-import DeleteLearnerForm from './components/admin/DeleteLearnerForm';
-import AddQuizFrom from './components/teacher/AddQuizForm';
-import LearnerDashboard from './components/learner/LearnerDashboard';
 
 function App() {
 
@@ -12,17 +7,6 @@ function App() {
       <h1>
         Welcome to TriPort, the best School Management System  
       </h1>
-
-      <LearnerDashboard />
-
-      <LearnersList />
-
-      <br></br>
-      <AddLearnerForm />
-
-      <br></br>
-      <AddQuizFrom />
-      <DeleteLearnerForm />
     </>
   )
 }
