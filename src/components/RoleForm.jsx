8ddefbeb804787c31d939f32f3ce912a3a7e54e6
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-04-03 11:22:18 +0200

    Add the basic unlinked buttons of the role form

--- src/components/RoleForm.jsx
+++ src/components/RoleForm.jsx
@@ -1,7 +1,28 @@
+import { use, useState } from "react";
+
 function RoleForm() {
+  const [isTeacher, setIsTeacher] = useState(false);
+  const [isLearner, setIsLearner] = useState(false);
+  const [isParent, setIsParent] = useState(false);
+
   return (
     <>
-      <h3>Choose your role: </h3>
+      <form>
+        <label>Choose Your Role:
+          <input
+            type="button"
+            value="Teacher"
+          />
+          <input
+            type="button"
+            value="Learner"
+          />
+          <input
+            type="button"
+            value="Parent"
+          />
+        </label>
+      </form>
     </>
   )
 }
