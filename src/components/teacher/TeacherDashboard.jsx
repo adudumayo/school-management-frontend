cad17cddfc893e7c0360e36c55b2b5dd841b3afc
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-05-06 12:13:26 +0200

    Add the logic to request login for teacher in the dashboard

--- src/components/teacher/TeacherDashboard.jsx
+++ src/components/teacher/TeacherDashboard.jsx
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+
 function TeacherDashboard() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginRequest = () => {
+    alert("You are logged in")
+  }
+  
   return (
     <>
       <h1>Hello, Teacher</h1>
+
+      <form onSubmit={loginRequest}>
+        <label>Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder='username'
+            required
+          />
+        </label>
+
+        <label>Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='password'
+            required
+          />
+        </label>
+        <input type="submit" />
+      </form>
     </>
   )
 }
