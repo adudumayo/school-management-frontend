b048f5baecb11893aaf7f8a4f86f10548e79a374
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-05-11 20:14:29 +0200

    Add basic input fields for the basic teacher attributes for the database, ONLY I CAN UNDERSTAND THIS COMMIT

--- src/components/admin/AddTeacherForm.jsx
+++ src/components/admin/AddTeacherForm.jsx
@@ -20,6 +20,28 @@ function AddTeacherForm() {
           onChange={(e) => setSurname(e.target.value)}
         />
       </label>
+      <label>Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>Title:
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="DROPDOWN"
+        />
+      </label>
+      <label>Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
       <input type='submit' />
     </form>
     
