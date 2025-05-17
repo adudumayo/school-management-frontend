6ea61c68d10670fd148b40263f4e7c688c0c482c
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-05-17 22:15:22 +0200

    Remove the routing related thingy from the TODOS, I have conquered it

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
 export default App
 // TODO
-// 1. Install the routing package and use it in the whole frontend
+// 1. 
 // 2. Figure out how to get the name or var and use it in another component
 // 3. Start taking care of the code and use identifiers in the html tags, make styling less headache
 // 4. Understand the Label in the form tag
